141f7e2630cbeaa36b0bc8ca5471bb565995b33d
riyer4 <riyer4@ucsc.edu>
2025-11-05 15:25:01 -0800

    added a bar to replace the fortune text

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -25,7 +25,11 @@ export default class GameScene extends Phaser.Scene {
 
         this.coin = new Coin(this, 200, 600, 'coin');
 
-        this.scoreText = this.add.text(20, 20, 'Fortune Gained: 0', { fontSize: '28px', fill: '#000', fontStyle: 'bold' });
+        this.fortuneBarBG = this.add.rectangle(200, 40, 300, 30, 0xAAAAAA).setOrigin(0, 0.5);
+        this.fortuneBar = this.add.rectangle(200, 40, 300 * 0.25, 30, 0xFFD903).setOrigin(0, 0.5);
+        this.fortuneLabel = this.add.text(50, 40, 'Fortune: ', { fontSize: '28px', fill: '#000', fontStyle: 'bold' }).setOrigin(0, 0.5);
+        this.fortuneValue = 0.25;
+
         this.messageText = this.add.text(20, 60, 'Click & drag to throw', { fontSize: '28px', fill: '#FFD903', fontStyle: 'bold' });
 
         this.dragLine = this.add.graphics();
@@ -42,7 +46,7 @@ export default class GameScene extends Phaser.Scene {
         this.midTier = this.add.image(950, 550, "midWater").setScale(0.5).setAlpha(0.5);
         this.bottomTier = this.add.image(950, 550, "bottomWater").setScale(0.5).setAlpha(0.5);
 
-        this.windArrow = this.add.triangle(50, 720, 0, 20, 10, 0, 20, 20, 0x00FFFF).setOrigin(0.5);
+        this.windArrow = this.add.image(80, 700, 'arrow').setOrigin(0.5).setScale(0.25);
         this.windText = this.add.text(50, 750, '0 mph', { fontSize: '24px', fill: '#000', fontStyle: 'bold' });
         this.stageText = this.add.text(600, 60, '20', { fontSize: '32px', fill: '#000', fontStyle: 'bold' });
         this.timerText = this.add.text(585, 15, 'Timer', { fontSize: '24px', fill: '#000', fontStyle: 'bold' });
@@ -53,8 +57,6 @@ export default class GameScene extends Phaser.Scene {
         this.stageDuration = 20000;
         this.stageNumber = 1;
         this.windForce = new Phaser.Math.Vector2(0, 0);
-
-        this.windArrow.setRotation(0); // start pointing upward
 
         this.stageTimer = this.time.addEvent({
             delay: this.stageDuration,
@@ -85,6 +87,10 @@ export default class GameScene extends Phaser.Scene {
             const dragEnd = new Phaser.Math.Vector2(pointer.x, pointer.y);
             let pullVec = this.dragStart.clone().subtract(dragEnd);
             this.coin.launch(pullVec);
+
+            this.fortuneValue -= 0.25 / 8;
+            if (this.fortuneValue < 0) this.fortuneValue = 0;
+            this.updateFortuneBar();
         });
 
         this.matter.world.on('collisionstart', event => {
@@ -96,12 +102,15 @@ export default class GameScene extends Phaser.Scene {
                         const tierScore = b.gameObject.score;
                         this.sound.play('ding', { volume: 0.2, rate: 1.5 });
                         this.sound.play('splash', { volume: 0.7, rate: 1.5 });
-                        this.score += tierScore;
                         this.coin.hasScored = true;
-                        this.scoreText.setText('Fortune Gained: ' + this.score);
                         this.messageText.setText('Score! Wish received').setColor("#00FF0D");
                         this.showScorePopup(tierScore, this.coin.x, this.coin.y);
                         this.coin.reset();
+
+                        const incrementMap = {1: 1/32, 3: 3/32, 5: 5/32};
+                        this.fortuneValue += incrementMap[tierScore] || 0;
+                        if (this.fortuneValue > 1) this.fortuneValue = 1;
+                        this.updateFortuneBar();
                     }
                 });
             });
@@ -112,7 +121,6 @@ export default class GameScene extends Phaser.Scene {
             .setInteractive()
             .on('pointerdown', () => this.resetGame());
 
-        this.nextStage();
     }
 
     update(time, delta) {
@@ -128,11 +136,11 @@ export default class GameScene extends Phaser.Scene {
 
         if (cx + r < 0 || cx - r > SCREEN_WIDTH || cy - r > SCREEN_HEIGHT || cy + r < 0) {
             this.coin.reset();
-            this.sound.play('lose', { volume: 0.4, rate: 1.5 });
+            this.sound.play('lose', { volume: 0.2, rate: 1.5 });
             this.messageText.setText('Miss :(').setColor("#BD0202");
         }
 
-        this.windArrow.setRotation(this.windForce.length() === 0 ? -Math.PI / 2 : this.windForce.angle() - Math.PI / 2);
+        this.windArrow.setRotation(this.windForce.angle());
         this.windText.setText(Math.round(this.windForce.length()) + ' mph');
         this.stageText.setText(Math.ceil(this.stageTimer.getRemaining() / 1000));
     }
@@ -147,13 +155,13 @@ export default class GameScene extends Phaser.Scene {
 
     resetGame() {
         this.coin.reset();
-        this.score = 0;
-        this.scoreText.setText('Fortune Gained: 0');
-        this.messageText.setText('Click & drag to throw').setColor("#FFD903");
+        this.fortuneValue = 0.25;
+        this.updateFortuneBar();
         this.stageNumber = 1;
         this.windForce.set(0, 0);
-        this.windArrow.setRotation(-Math.PI / 2);
+        this.windArrow.setRotation(0);
         this.windText.setText('0 mph');
+        this.messageText.setText('Click & drag to throw').setColor("#FFD903");
     }
 
     showScorePopup(value, x, y) {
@@ -167,6 +175,9 @@ export default class GameScene extends Phaser.Scene {
             onComplete: () => popup.destroy()
         });
     }
-}
 
+    updateFortuneBar() {
+        this.fortuneBar.width = 300 * this.fortuneValue;
+    }
+}
 
